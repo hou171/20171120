cbb223e2820f3c583af744a829d3376f32ebec08
I432 <jimmy212tw@gmail.com>
2017-11-20 16:02:21 +0800

    123

--- src/star2.java
+++ src/star2.java
@@ -5,7 +5,7 @@ public class star2 {
         Scanner sc = new Scanner(System.in);
         Integer a,b;
         for(a=1;a<=5;a++){
-            for(b=1;b<=a;b++){
+            for(b=5;b>=a;b--){
                 System.out.print("*");
             }
             System.out.print("\n");
---
+++ src/three.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class three {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        Integer i,j;
+
+        for(j=2;j<=9;j++){
+            for(i=1;i<=9;i++){
+                System.out.println(j+"x"+i+"="+j*i);
+        }
+        }
+        System.out.print("\n");
+    }
+}
